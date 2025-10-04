eecd826a2b7b578a3c146028ffe7d80a4647c4c7
dhruvd22 <dhruvdahiya@gmail.com>
2025-10-04 10:26:37 +0100

    tabcmt

--- projectplanner/ui/pages/_app.tsx
+++ projectplanner/ui/pages/_app.tsx
@@ -1,6 +1,11 @@
-﻿import type { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { AppLayout } from "@/components/AppLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
